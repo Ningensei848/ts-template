ecc15799900f74a174509d91b69501c7152b6b05
Ningensei848 <k.kubokawa@klis.tsukuba.ac.jp>
2021-02-04 18:11:27 +0900

    fix / editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ module.exports = {
         },
         { to: 'blog', label: 'Blog', position: 'left' },
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/Ningensei848/ts-template',
           label: 'GitHub',
           position: 'right',
         },
@@ -51,16 +51,12 @@ module.exports = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: 'Zenn.dev',
+              href: 'https://zenn.dev/ningensei848',
             },
             {
               label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              href: 'https://twitter.com/Ningensei848',
             },
           ],
         },
@@ -72,7 +68,7 @@ module.exports = {
               to: 'blog',
             },
             {
-              label: 'GitHub',
+              label: 'Docusaurus',
               href: 'https://github.com/facebook/docusaurus',
             },
           ],
@@ -89,13 +85,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/Ningensei848/ts-template/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/Ningensei848/ts-template/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
